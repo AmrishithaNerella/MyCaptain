6c17c82e3d8209a8a5fa03bd4996eec065d2a6d8
AmrishithaNerella <50817016+AmrishithaNerella@users.noreply.github.com>
2020-10-29 19:42:18 +0530

    Create Mississippi.py

---
+++ Mississippi.py
@@ -0,0 +1,12 @@
+W="MISSISSIPPI"
+print("I=",end="")
+print(W.count("I"))
+
+print("S=",end="")
+print(W.count("S"))
+
+print("M=",end="")
+print(W.count("M"))
+
+print("P=",end="")
+print(W.count("P"))
